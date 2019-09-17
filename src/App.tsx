164fd9a5af4d686edb4affebaa8063068e051a06
Mikael Malmqvist <mikael@malmqvist.it>
2019-09-17 14:05:09 +0200

    Add routing to dinners and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Landing from "./routes/Landing";
+import Dinners from "./routes/Dinners";
+
+const NotFound = () => <h2>Not found</h2>;
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dinners" component={Dinners} />
+        <Route exact path="/*" component={NotFound} />
+      </Switch>
     </div>
   );
 };
---
+++ src/routes/Dinners.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dinners = () => {
+  return <h2>Dinners page</h2>;
+};
+
+export default Dinners;
