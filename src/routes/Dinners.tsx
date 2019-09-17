af2692f8ace0755ca042e01b38d34d2a6c9f6098
Ante Wall <antewall@gmail.com>
2019-09-17 18:29:49 +0200

    feat: api works

--- src/routes/Dinners.tsx
+++ src/routes/Dinners.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import DinnerList from "./components/DinnerList";
-import AddButton from "./components/AddButton";
-import ProductList from "./components/ProductList";
-import AddItemsDialog from "./components/AddItemsDialog";
-import useSuggestions from "../hooks/useSuggestions";
-import { useIngredients } from "../hooks/useIngredients";
-import { mockedDinners } from "../utils";
+import React, { useEffect, useState } from 'react';
+import DinnerList from './components/DinnerList';
+import AddButton from './components/AddButton';
+import ProductList from './components/ProductList';
+import AddItemsDialog from './components/AddItemsDialog';
+import useSuggestions from '../hooks/useSuggestions';
+import { useIngredients } from '../hooks/useIngredients';
+import { mockedDinners } from '../utils';
 
 const Dinners = () => {
   const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
@@ -18,8 +18,8 @@ const Dinners = () => {
   return (
     <div>
       <h2>Your Expyro 🐉</h2>
-      <div style={{ display: "flex", padding: "20px" }}>
-        <DinnerList dinners={mockedDinners} />
+      <div style={{ display: 'flex', padding: '20px' }}>
+        <DinnerList dinners={suggestions} />
         <ProductList products={ingredients} />
       </div>
       <AddItemsDialog
